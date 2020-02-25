2b9f88d4f2baa30d00a63aaadbccf68bca8e4829
VashchenkoA <61081442+VashchenkoA@users.noreply.github.com>
2020-02-25 19:42:00 +0500

    external app final cut

--- externalApp/externalApp.cpp
+++ externalApp/externalApp.cpp
@@ -7,25 +7,26 @@ int main()
 {
 	string fileName;
 
-	// TODO: validation
+	// Simple dialog with user
 	while (1)
 	{
-		cout << "Please enter disk name. Should be like C, D, and so on" << endl;
+		cout << "Please enter disk name. (One letter: A, B, C)" << endl;
 		fileName = cin.get();
 		if (fileName.length() == 1)
 		{
 			break;
 		}
 	}
-
+	//Converting to string with right format
 	string fileNameFormated = "\\\\.\\" + fileName + ":";
 
+	//Struct call
 	BOOT_NTFS pBootRecord;
 
+	//Function call
 	if (bootInfo(fileNameFormated, &pBootRecord))
 	{
 		PrintBootSectInfo(pBootRecord);
 	}
-
 
 }
