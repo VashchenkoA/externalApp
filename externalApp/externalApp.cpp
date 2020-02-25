7d7831231fd5421d54984c5affe83db784cdc2e4
VashchenkoA <61081442+VashchenkoA@users.noreply.github.com>
2020-02-25 17:26:47 +0500

    Добавьте файлы проекта.

---
+++ externalApp/externalApp.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include "bootLoader.h"
+
+using namespace std;
+
+int main()
+{
+	string fileName;
+
+	// TODO: validation
+	while (1)
+	{
+		cout << "Please enter disk name. Should be like C, D, and so on" << endl;
+		fileName = cin.get();
+		if (fileName.length() == 1)
+		{
+			break;
+		}
+	}
+
+
+	string fileNameFormated = "\\\\.\\" + fileName + ":";
+
+
+	BOOT_NTFS pBootRecord;
+
+	if (bootInfo(fileNameFormated, &pBootRecord))
+	{
+		PrintBootSectInfo(pBootRecord);
+	}
+
+
+}
