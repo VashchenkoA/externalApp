c82009319e468dc567ba84fed027303ac0a8ab8b
VashchenkoA <61081442+VashchenkoA@users.noreply.github.com>
2020-04-26 15:26:02 +0500

    final version for class-oriented DLL

--- externalApp/externalApp.cpp
+++ externalApp/externalApp.cpp
@@ -1,41 +1,20 @@
 ﻿#include <iostream>
-#include "bootLoader.h"
-
-using namespace std;
+#include "FileSystemClass.h"
+#include "pch.h"
+#include <stdio.h>
+#include <windows.h>
 
 int main()
 {
-	WCHAR fileName;
-
-	// Simple dialog with user
-	while (1)
+	if (getDevName)
 	{
-		cout << "Please enter disk name. (One letter: A, B, C)" << endl;
-		fileName = getchar();
-		if (wcslen(&fileName) == 1)
-		{
-			break;
-		}
+		getDevName(); 
+	}
+	else
+	{
+		perror("Error: ");
 	}
 
-	WCHAR buffer[11];
-	wcscat_s(buffer, L"\\\\.\\");
-	wcscat_s(buffer, &fileName);
-
-	//Converting to string with right format
-	const WCHAR fileNameFormated = wcscat_s(buffer,L":");
-	
-	wprintf(&fileNameFormated);
-
-	//Struct call
-	BOOT_NTFS pBootRecord;
-
-	//Function call
-	if (bootInfo(&fileNameFormated, &pBootRecord))
-	{
-		PrintBootSectInfo(pBootRecord);
-	}
-	
 	system("pause");
 	return 0;
 }
